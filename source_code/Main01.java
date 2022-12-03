0149ec579844865832db7a1545dcf8b75edeaca8
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-03 19:15:41 +0200

    Create Main01.java

---
+++ source_code/Main01.java
@@ -0,0 +1,15 @@
+// file name: Main01.java
+// package name: javaintro
+// project name: JavaIntro
+
+package javaintro;
+
+// the class name (Main01) should be the
+// same with the file name (Main01.jeva)
+
+public class Main01 {
+    public static void main(String[] args) {
+        System.out.println("Hi there!!!");
+    }
+    
+}
