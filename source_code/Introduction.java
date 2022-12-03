f46d86875758205390a653b644f9f212de638df4
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-04 00:35:26 +0200

    Update and rename Main01.java to Introduction.java

---
+++ source_code/Introduction.java
@@ -0,0 +1,12 @@
+// file name: Introduction.java
+//
+
+// the class name (Introduction) should be the
+// same with the file name (Introduction.java)
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Hi there!!!");
+    }
+    
+}
