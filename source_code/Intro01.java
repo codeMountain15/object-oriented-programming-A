2094eb7e82a5428b4136dba46f3a6c510d693cab
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-04 09:07:07 +0200

    Create Intro01.java

---
+++ source_code/Intro01.java
@@ -0,0 +1,19 @@
+// file name: Intro01.java
+//
+
+public class Intro01 {
+    public static void main(String[] args) {
+        
+    int fInteger = 5;
+    int sInteger = 10;
+    float aFloat = 9.99f;
+    char aLetter = '!';
+    boolean aBool = true;
+    String aText = "Hello everybody";
+        
+    System.out.println(aText + aLetter);
+    System.out.println("The sum is: ");
+    System.out.println(fInteger + sInteger);
+    }
+    
+}
