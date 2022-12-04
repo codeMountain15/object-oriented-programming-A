9a91e4c6f7f118d5cc2e51dac1d49306c99de466
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-04 20:35:29 +0200

    Create Wrapper01.java

---
+++ source_code/Wrapper01.java
@@ -0,0 +1,13 @@
+// file name: Wrapper01.java
+//
+
+public class Wrapper01 {
+    public static void main(String[] args) {
+
+    Double doubleObj = 1.99;
+    Character charObj = '€';
+    System.out.println(doubleObj.toString() + charObj);
+    
+  }
+    
+}
