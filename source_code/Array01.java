835e8600a60e0fe1c82ec99e2d91b004a27578bc
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-04 17:30:58 +0200

    Create Array01.java

---
+++ source_code/Array01.java
@@ -0,0 +1,17 @@
+// file name: Array01.java
+//
+
+public class Array01 {
+    public static void main(String[] args) {
+        
+    int[] numCollection= new int[6];
+    
+    for (int i = 0; i < numCollection.length; i++)
+        numCollection[i] = i;
+    
+    for (int i = 0; i < numCollection.length; i++)
+        System.out.println(numCollection[i]);
+    
+    }
+    
+}
